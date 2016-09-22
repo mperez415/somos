9e795ab1ef7515f4a1f5b1bc805395527ae6535e
Mario Perez <mario.perez415@gmail.com>
2016-09-22 04:53:31 -0400

    finish up cover-page styles

--- scripts/cover-page.js
+++ scripts/cover-page.js
@@ -1,62 +1,59 @@
-/******************************************************
+/* ====================================================
   This is the entrypoint to the JS in your app.
   ES6 features are supported inside this file.
-******************************************************/
+==================================================== /
 
 /*
  ** Requires
-******************************************/
+ ******************************************/
 // jQuery
 window.jQuery = require('jquery');
 window.$ = window.jQuery; // for use in this file
 require('responsiveslides');
 
-
 /*
  ** Imports
-******************************************/
-import {Utility} from './modules/_utility.js';
-import { MdnzrExtends } from './modules/_modernizrExtends.js';
+ ******************************************/
+import {
+  Utility
+} from './modules/_utility.js';
+import {
+  MdnzrExtends
+} from './modules/_modernizrExtends.js';
 
 /*
  ** ok to load before DOM Ready
-******************************************/
+ ******************************************/
 MdnzrExtends.init(); // Start modernizer extensions
 
 /*
  ** jQuery Document Ready
-******************************************/
-$(document).ready(function() {
-
+ ******************************************/
+$(document).ready(function () {
   /*
-  ** Load all Images
-  *****************************/
+   ** Load all Images
+   *****************************/
   Utility.loadAllImages();
 
   /*
-  ** Replace svgs with pngs
-  ** if svgs are not supported.
-  *****************************/
-  if (!Modernizr.svg) {
-      var imgs = document.getElementsByTagName('img');
-      var svgExtension = /.*\.svg$/ ;
-      var l = imgs.length;
-      for(var i = 0; i < l; i++) {
-          if(imgs[i].src.match(svgExtension)) {
-              imgs[i].src = imgs[i].src.slice(0, -3) + 'png';
-              console.log(imgs[i].src);
-          }
-      }
-  }
+   ** Start background Slider
+   ****************************/
+  $('#background-wrapper').responsiveSlides();
 
-  /*
-  ** Start background Slider
-  ****************************/
-  $("#background-wrapper").responsiveSlides();
+  $(window).on('load', function () {
+    /*
+     ** Show popover link
+     ****************************/
+    $('#popover-link').animate({
+      opacity: 1
+    }, 'slow', function () {
+      // Animation complete.
+    });
+  });
 });
 
 /*
  ** js event subscriptions
-******************************************/
+ ******************************************/
 // The event subscription that reloads images on window resize
 window.addEventListener('resize', Utility.loadAllImages);
